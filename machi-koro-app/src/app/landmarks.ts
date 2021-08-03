3815b44b9f391fc167f9fc1b1e572f6739bb8e5b
Shannon Brown <shannon.theoret@outlook.com>
2021-08-02 20:51:39 -0400

    Add tooltip descriptions to landmark buttons

--- machi-koro-app/src/app/landmarks.ts
+++ machi-koro-app/src/app/landmarks.ts
@@ -2,21 +2,25 @@ export const LANDMARKS: Landmark[] =
   [ {
       id: "train",
       name: "Train Station",
+      description: "Roll 2 dice.",
       cost: 4
     },
     {
       id: "shopping",
       name: "Shopping Mall",
+      description: "Earn +1 coin from your own cup and bread establishments.",
       cost: 10
     },
     {
       id: "amusement",
       name: "Amusement Park",
+      description: "If you roll matching dice, take another turn after this one.",
       cost: 16
     },
     {
       id: "radio",
       name: "Radio Tower",
+      description: "Once every turn you can choose to re-roll your dice.",
       cost: 22
     }
 
@@ -25,5 +29,6 @@ export const LANDMARKS: Landmark[] =
 export class Landmark {
   id: string;
   name: string;
+  description: string;
   cost: number;
 }
